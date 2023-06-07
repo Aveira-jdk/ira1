51e526caef843d57d1bb4bca4e48ab7f9ded665c
Aveira <kirito.html@gmail.com>
2023-06-07 15:31:19 +0400

    space

--- src/main/java/az/iktlab/hackerrank/Solution.java
+++ src/main/java/az/iktlab/hackerrank/Solution.java
@@ -1,26 +1,29 @@
 package az.iktlab.hackerrank;
-
-import java.io.*;
-import java.util.*;
-
-
 import java.util.Scanner;
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+}
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String A = sc.next();
-        String B = sc.next();
-        int lengthAll = A.length() + B.length();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
 
-        String capitalizedSt1 = Character.toUpperCase(A.charAt(0)) + A.substring(1);
-        String capitalizedSt2 = Character.toUpperCase(B.charAt(0)) + B.substring(1);
-        String result = capitalizedSt1 + " " + capitalizedSt2;
+        while (n-- != 0) {
+            String userName = scan.nextLine();
 
-        System.out.println(lengthAll);
-        System.out.println("No");
-        System.out.println(result);
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
 
-        sc.close();
+        scan.close();
     }
 }
